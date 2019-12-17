34fa300567799cc5cd69a9c903067b0f930e34bb
kewl-deus <deus.mail@web.de>
2019-12-17 19:52:49 +0100

    hal explorer & webflux

--- customer-service/src/main/java/com/example/customerservice/repository/CustomerIndex.java
+++ customer-service/src/main/java/com/example/customerservice/repository/CustomerIndex.java
@@ -2,8 +2,9 @@ package com.example.customerservice.repository;
 
 import com.example.customerservice.model.entity.CustomerIndexRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource
 public interface CustomerIndex extends JpaRepository<CustomerIndexRecord, Long> {
 }
